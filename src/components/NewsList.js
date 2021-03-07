d0d30fe8c8037a967ef55f8c6bd695c761eb3176
muu86 <mu86@naver.com>
2021-03-07 09:22:27 +0900

    book study

---
+++ src/components/NewsList.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const response = await axios.get(
+                    `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=90ec521990374788a3f1bc7f807ea44a`
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [category]);
+
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>;
+    }
+
+    if (!articles) {
+        return null;
+    }
+
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
